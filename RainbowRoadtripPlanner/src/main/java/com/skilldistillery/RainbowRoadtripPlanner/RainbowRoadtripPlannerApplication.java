5955ac3a7504a42c83ef3487f4c0d99493e45043
Collin Wheat <collinwheat@gmail.com>
2023-04-18 13:21:06 -0400

    Configure Spring Security

--- RainbowRoadtripPlanner/src/main/java/com/skilldistillery/RainbowRoadtripPlanner/RainbowRoadtripPlannerApplication.java
+++ RainbowRoadtripPlanner/src/main/java/com/skilldistillery/RainbowRoadtripPlanner/RainbowRoadtripPlannerApplication.java
@@ -2,12 +2,20 @@ package com.skilldistillery.RainbowRoadtripPlanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class RainbowRoadtripPlannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RainbowRoadtripPlannerApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+	   return new BCryptPasswordEncoder();
 	}
 
 }
