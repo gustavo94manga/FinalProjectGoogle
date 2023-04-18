5924f64d95ecbb812813a2642f77875047dfa6e1
kennyan11b <kennyan11b@gmail.com>
2023-04-18 13:42:26 -0700

    install ng-bootstrap

--- ngRainbowRoadPlanner/src/app/app.module.ts
+++ ngRainbowRoadPlanner/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
