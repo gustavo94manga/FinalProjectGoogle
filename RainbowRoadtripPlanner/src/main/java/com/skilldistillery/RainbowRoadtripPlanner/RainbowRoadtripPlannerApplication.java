41c12b80ac6e4b0d8baf9acf97d5ab6171d61e70
Collin Wheat <collinwheat@gmail.com>
2023-04-18 13:07:35 -0400

    Create Spring Boot Project

---
+++ RainbowRoadtripPlanner/src/main/java/com/skilldistillery/RainbowRoadtripPlanner/RainbowRoadtripPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.RainbowRoadtripPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowRoadtripPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowRoadtripPlannerApplication.class, args);
+	}
+
+}
---
+++ RainbowRoadtripPlanner/src/test/java/com/skilldistillery/RainbowRoadtripPlanner/RainbowRoadtripPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.RainbowRoadtripPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowRoadtripPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
