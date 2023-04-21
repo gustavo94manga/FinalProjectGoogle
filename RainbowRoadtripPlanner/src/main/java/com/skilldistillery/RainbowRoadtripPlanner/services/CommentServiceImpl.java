e77624daf36b8dfc171e7dc99fcfaf70c38187ac
Angel Garza <gustavo94manga@gmail.com>
2023-04-21 18:10:46 -0500

    implementing comments crud and controllers

--- RainbowRoadtripPlanner/src/main/java/com/skilldistillery/RainbowRoadtripPlanner/services/CommentServiceImpl.java
+++ RainbowRoadtripPlanner/src/main/java/com/skilldistillery/RainbowRoadtripPlanner/services/CommentServiceImpl.java
@@ -1,21 +1,53 @@
 package com.skilldistillery.RainbowRoadtripPlanner.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.RainbowRoadtripPlanner.entities.Comment;
+import com.skilldistillery.RainbowRoadtripPlanner.entities.User;
 import com.skilldistillery.RainbowRoadtripPlanner.repositories.CommentRepository;
+import com.skilldistillery.RainbowRoadtripPlanner.repositories.UserRepository;
 
 @Service
 public class CommentServiceImpl implements CommentService {
 	
 	@Autowired
 	private CommentRepository commentRepo;
+	
+	@Autowired
+	private UserRepository userRepo;
 
 	@Override
-	public Comment getCommentByCommentId(int commentId) {
-		
+	public List<Comment> showAll(String username) {
+		return commentRepo.findByUser_Username(username);
+	}
+
+	@Override
+	public Comment show(String username, int id) {
+		return commentRepo.findById(id);
+	}
+
+	@Override
+	public Comment create(String username, Comment comment) {
+		User user = userRepo.findByUsername(username);
+		if(user != null) {
+			commentRepo.saveAndFlush(comment);
+		}
 		return null;
+	}
+
+	@Override
+	public Comment update(String username, int id, Comment comment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean destroy(String username, int id) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
