3b9c832002a7695a00d49a31dcea537d70d315a6
Angel Garza <gustavo94manga@gmail.com>
2023-04-21 11:01:16 -0500

    finished carousel

--- ngRainbowRoadPlanner/src/app/components/home/home.component.ts
+++ ngRainbowRoadPlanner/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
+import { Trip } from 'src/app/models/trip';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+
+  selected: Trip | null = null;
+  activeSlide = 0;
+
+  prevSlide() {
+    this.activeSlide = (this.activeSlide - 1 +5) % 5;
+  }
+
+  nextSlide() {
+    this.activeSlide = (this.activeSlide + 1 + 5) % 5;
+  }
+
+  displayTripImages(tripImage: Trip) {
+    this.selected = tripImage;
+  }
+
 
 }
