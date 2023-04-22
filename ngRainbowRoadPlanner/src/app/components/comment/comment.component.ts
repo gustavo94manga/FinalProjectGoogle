ccb0a29479a01772cb94aa9279304ae8e411eecb
Angel Garza <gustavo94manga@gmail.com>
2023-04-22 17:02:06 -0500

    stub out comment ts

--- ngRainbowRoadPlanner/src/app/components/comment/comment.component.ts
+++ ngRainbowRoadPlanner/src/app/components/comment/comment.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { CommentService } from './../../services/comment.service';
 import { Component } from '@angular/core';
 import { Comment } from 'src/app/models/comment';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-comment',
@@ -13,34 +14,102 @@ export class CommentComponent {
 
 
   selected: Comment | null = null;
-newTrip: Comment = new Comment();
+newComment: Comment = new Comment();
+editComment: Comment | null = null;
+comments: Comment[] = [];
 
 
 
 constructor(private commentService: CommentService,
   private auth: AuthService,
   private route: ActivatedRoute,
-  private router: Router,){
-
+  private router: Router,
+  private datePipe: DatePipe) {
+  {
+  }
 }
 
+ngOnInit() {
+  let commentIdString = this.route.snapshot.paramMap.get('id');
+  if (commentIdString) {
+    let id = parseInt(commentIdString)
+    if(isNaN(id)) {
+      this.router.navigateByUrl('invalidId');
+    }
+    else {
+      this.commentService.show(id).subscribe({
+        next: (comment) => {
+          this.selected = comment;
+        },
+        error: (fail) => {
+          this.router.navigateByUrl('Comment Not Found');
+        }
+      })
+    }
+  }
+}
 
-createTrip(comment: Comment){
+displayComment(comment: Comment) {
+  this.newComment = comment;
+}
+
+displayTable() {
+  this.selected = null;
+}
+
+createComment(comment: Comment): void{
   this.commentService.create(comment).subscribe({
     next:(madeComment)=>{
-      this.selected=madeComment;
-      console.log(madeComment)
+      this.newComment = new Comment();
+      this.reload();
+    },
+    error: (fail) => {
+      console.error('Error creating comment');
     }
+  });
+}
 
+setEditComment() {
+  this.editComment = Object.assign({}, this.selected);
+}
+
+updateComment(comment: Comment, goToDetail = true){
+  this.commentService.update(comment).subscribe({
+    next:(updatedComment)=>{
+    this.editComment = null;
+    if(goToDetail) {
+      this.selected = updatedComment;
+    }
+    this.reload();
+    },
+    error: (fail) => {
+      console.error('Error updating comment');
+      console.log(fail);
+    }
   })
 }
 
-updateTrip(comment: Comment){
-  this.commentService.update(comment).subscribe({
-    next:(updatedComment)=>{
-    this.selected=updatedComment;
+deleteComment(id: number){
+  this.commentService.destroy(id).subscribe({
+    next:(result) => {
+      this.reload();
+    },
+    error: (fail) => {
+      console.error('Error deleting comment');
+      console.error(fail);
     }
-  })
+  });
+}
+reload() {
+  this.commentService.index().subscribe({
+  next: (comments) => {
+    this.comments = comments;
+},
+    error: (fail) => {
+      console.error('Error getting comment list from service');
+      console.error(fail);
+    }
+  });
 }
 
 }
